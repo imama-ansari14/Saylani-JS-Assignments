5c5986742db8c7fe2f1f2b1c3021f511129b3b6c
Imama Ansari <ansariimama@yahoo.com>
2025-05-20 16:39:23 +0500

    Alhumdulillah Done

--- Basic Calculator In Table/app.js
+++ Basic Calculator In Table/app.js
@@ -6,3 +6,19 @@ let sub = a - b;
 let mul = a * b;
 let div = a / b;
 let mod = a % b;
+
+document.write(`<table style="border: 4px solid black"> 
+    <tr  style="background-color: rgb(158, 158, 212);"> <th>Addition</th>
+    <th>Subtraction</th>
+    <th>Multiplication</th>
+    <th>Division</th>
+    <th>Modulus</th>
+    </tr>`+ `<tr  style="background-color: gainsboro;">
+        <td>`+add+`</td>
+        <td>`+sub+`</td>
+        <td>`+mul+`</td>
+        <td>`+div+`</td>
+        <td>`+mod+`</td>
+    </tr>
+   </table>
+`);
